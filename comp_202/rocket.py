aa33cb13466152fce5b5baabd61b552eda8e68d5
1daskalos1 <yklironomos.jac@gmail.com>
2024-09-30 09:13:15 -0400

    rocket.py mod -> questions

--- comp_202/rocket.py
+++ comp_202/rocket.py
@@ -20,15 +20,27 @@ def rocket_volume(radius, height_cone, height_cyl):
     return rounded_combined_volume
 
 def rocket_area(radius, height_cone, height_cyl):
-    area_cone = m.pi * radius * (radius + m.sqrt(height_cone ** 2 + radius ** 2))
+    area_cone = m.pi * radius * \
+                (radius + m.sqrt(height_cone ** 2 + radius ** 2))
     area_cyl = 2 * m.pi * radius * (height_cyl + radius)
     area_circle = m.pi * radius ** 2
     exact_area_rocket = area_cone + area_cyl - 2 * area_circle
     rounded_area_rocket = round(exact_area_rocket, 2)
     return rounded_area_rocket
 
-def rocket_mass(radius, height_cone, height_cyl):
-    
-    return total_mass_rocket
+def rocket_mass(radius, height_cone, height_cyl):           
+    exact_rocket_mass = rocket_volume(radius, height_cone, height_cyl) * 1.225
+    rounded_rocket_mass = round(exact_rocket_mass, 2)
+    return rounded_rocket_mass
     
 
+def rocket_fuel(radius, height_cone, height_cyl, velocity_e, velocity_i, time):
+    tsiolkovsky_rocket_eqn = rocket_mass(radius, height_cone, height_cyl) * \
+                             (m.e ** (velocity_i/elocity_e) - 1)
+    if rounded_rocket_mass < 100000:
+        return 1360
+    if rounded_rocket_mass >= 100000 and rounded_rocket_mass < 400000:
+        return 2000
+    return 2721 #there is no need for if-else since each case returns a value
+    total_mass  = rocket_mass()
+    return total_fuel_needed
